852ac03e2782a77b6ee51f72194758a44c43a47b
Weiting-Zhang <zhangwei_t@outlook.com>
2020-01-02 17:38:11 +0800

    add updateDomProperties

--- src/reconciler.js
+++ src/reconciler.js
@@ -8,14 +8,50 @@ export function render(element, container) {
 
 function reconcile(parentDom, instance, element) {
   if (instance == null) {
-    const newInstance = instantiate(element);
-    parentDom.appendChild(newInstance.dom);
-    return newInstance;
+    debugger;
+    const newInstance = instantiate(element); // 第一次实例化完成（包含 childInstances)
+    parentDom.appendChild(newInstance.dom); // 此时的 dom，已经收集好了所有子元素
+    return newInstance; // 返回并存储到 rootInstance 中，供下次 render 时使用
   } else {
     const newInstance = instantiate(element);
-    parentDom.replaceChild(newInstance.dom, instance.dom);
-    return newInstance;
+    parentDom.replaceChild(newInstance.dom, instance.dom); // 暴力替换
+    return newInstance; // 返回并存储到 rootInstance 中，供下次 render 时使用
   }
+}
+
+function updateDomProperties(dom, prevProps, nextProps) {
+  const isEvent = name => name.startsWith("on");
+  const isAttribute = name => !isEvent(name) && name != "children";
+
+  // Remove event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .forEach(name => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[name]);
+    });
+
+  // Remove attributes
+  Object.keys(prevProps)
+    .filter(isAttribute)
+    .forEach(name => {
+      dom[name] = null;
+    });
+
+  // Set attributes
+  Object.keys(nextProps)
+    .filter(isAttribute)
+    .forEach(name => {
+      dom[name] = nextProps[name];
+    });
+
+  // Add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .forEach(name => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, nextProps[name]);
+    });
 }
 
 function instantiate(element) {
@@ -27,25 +63,16 @@ function instantiate(element) {
     ? document.createTextNode("")
     : document.createElement(type);
 
-  // Add event listeners
-  const isListener = name => name.startsWith("on");
-  Object.keys(props).filter(isListener).forEach(name => {
-    const eventType = name.toLowerCase().substring(2);
-    dom.addEventListener(eventType, props[name]);
-  });
-
-  // Set properties
-  const isAttribute = name => !isListener(name) && name != "children";
-  Object.keys(props).filter(isAttribute).forEach(name => {
-    dom[name] = props[name];
-  });
+  updateDomProperties(dom, [], props);
 
   // Instantiate and append children
   const childElements = props.children || [];
   const childInstances = childElements.map(instantiate);
-  const childDoms = childInstances.map(childInstance => childInstance.dom);
-  childDoms.forEach(childDom => dom.appendChild(childDom));
+  childInstances.forEach(child => {
+    dom.appendChild(child.dom);
+  });
 
+  // return to reconcile
   const instance = { dom, element, childInstances };
   return instance;
 }
